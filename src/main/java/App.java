8c50579c2cb85d225fd60e46cd936df22447dc75
iano <ianmwema07@gmail.com>
2022-05-02 22:29:37 +0300

    add all imports

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import models.Hero;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
 public class App {
+    public static void main(String[] args) {
+
+    }
 }
