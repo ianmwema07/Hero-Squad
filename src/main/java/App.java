cca22270276f83dd1d9202f0a7e91fa708f20e9d
iano <ianmwema07@gmail.com>
2022-05-04 15:02:03 +0300

    add connection to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,13 +15,28 @@ import spark.Request;
 import spark.Response;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
+
+
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
+
         staticFileLocation("/public");
         String connectionString = "jdbc:postgresql://localhost:5432/todolist"; //connect to todolist, not todolist_test!
         Sql2o sql2o = new Sql2o(connectionString, null, null);
         Sql2oHeroDao heroDao = new Sql2oHeroDao(sql2o);
         Sql2oSquadDao squadDao = new Sql2oSquadDao(sql2o);
+
+
 
         //get: show all tasks in all categories and show all categories
         get("/", (req, res) -> {
